03b4b2bf977996a3b724525c12393cac8ea7ee20
arjava <arjava379@gmail.com>
2022-06-28 14:48:10 +0700

    test_while_updating_todo_list_name_field_is_required

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -2,10 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TodoList;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class TodoListController extends Controller
 {
     public function index()
     {
-        return response(['list'=>[]]);
+        $list = TodoList::all();
+        return response($list);
+    }
+
+    public function show(TodoList $list)
+    {
+        // $list = TodoList::findOrFail($todolist);
+        return response($list);
+    }
+
+    public function store(Request $request){
+        // $store = TodoList::created(['name' => 'my list']);
+        $request->validate(['name'=>['required']]);
+        // $list = 
+        return TodoList::create($request->all());
+        // return response($list, Response::HTTP_CREATED);
+        // return $list;
+    }
+
+    public function destroy(TodoList $list){
+        $list->delete();
+        return response('',Response::HTTP_NO_CONTENT);
+    }
+
+    public function update(Request $request, TodoList $list){
+        $request->validate(['name' => ['required']]);
+        $list->update($request->all());
+        return response($list);
     }
 }
--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,25 +2,92 @@
 
 namespace Tests\Feature;
 
+use App\Models\TodoList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
-    
-    public function test_store_todo_list()
+
+    use RefreshDatabase;
+
+    public function setup():void
     {
-        
+        parent::setUp();
+        $this->list = TodoList::factory()->create(['name'=>'my list']);
+    }
+    
+    public function test_fetch_all_todo_list()
+    {
+
         // preparation / prepare
+        // $list = 
+        // TodoList::factory()->count(2)->create(['name'=>'my list']);
+        // dd($list);
         
         // action / perform
 
-        // dd(route('todo-list.index'));
-        $response = $this->getJson(route('todo-list.store'));
+        // dd(route('todo-list.store'));
+        $response = $this->getJson(route('todo-list.index'));
+        // dd($response->json()[0]['name]);
 
         // assertion / predict
 
         $this->assertEquals(1,count($response->json()));
+        $this->assertEquals('my list',$response->json()[0]['name']);
+        
+    }
+
+    public function test_fetch_single_todo_list()
+    {
+        // $list = TodoList::factory()->create(['name'=>'my list']);
+        $response = $this->getJson(route('todo-list.show',$this->list->id))
+                    ->assertOk()
+                    ->json();
+
+        $this->assertEquals($response['name'], $this->list->name);
+    }
+
+    public function test_store_new_todo_list()
+    {
+        $list = TodoList::factory()->make();
+        $response = $this->postJson(route('todo-list.store'),['name' => $list->name])
+            ->assertCreated()
+            ->json();
+        // dd($response['name']);
+        $this->assertEquals($list->name, $response['name']);
+        $this->assertDatabaseHas('todo_lists', ['name' => $list->name]);
+    }
+
+    public function test_while_storing_todo_list_name_field_is_required(){
+        $this->withExceptionHandling();
+        $response = $this->postJson(route('todo-list.store'))
+                    ->assertUnprocessable()
+                    ->assertJsonValidationErrors(['name']);
+        // dd($response);
+    }
+
+    public function test_delete_todo_list(){
+        // dd($this->list->name);
+        $this->deleteJson(route('todo-list.destroy', $this->list->id))
+                ->assertNoContent();
+
+        $this->assertDatabaseMissing('todo_lists', ['name' => $this->list->name]);
+    }
+
+    public function test_update_todo_list(){
+        $this->patchJson(route('todo-list.update', $this->list->id),['name' => 'updated name'])
+        ->assertOk();
+
+        $this->assertDatabaseHas('todo_lists', ['id' => $this->list->id, 'name' => 'updated name']);
+    }
+
+    public function test_while_updating_todo_list_name_field_is_required(){
+        $this->withExceptionHandling();
+        $response = $this->patchJson(route('todo-list.update', $this->list->id))
+                    ->assertUnprocessable()
+                    ->assertJsonValidationErrors(['name']);
+        // dd($response);
     }
 }
