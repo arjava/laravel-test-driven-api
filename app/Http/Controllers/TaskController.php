a0b3579af7d36eb3419d1a95d778483098d3980e
arjava <arjava379@gmail.com>
2022-06-30 16:51:02 +0700

    writing has many relationship

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,14 +10,12 @@ use Symfony\Component\HttpFoundation\Response;
 class TaskController extends Controller
 {
     public function index(TodoList $todo_list){
-        $tasks = Task::where(['todo_list_id' => $todo_list->id])->get();
+        $tasks = $todo_list->tasks;
         return response($tasks);
     }
 
     public function store(Request $request, TodoList $todo_list){
-        $request['todo_list_id'] = $todo_list->id;
-        $task = Task::create($request->all());
-        return $task;
+        return $todo_list->tasks()->create($request->all());
     }
 
     public function destroy(Task $task){
