f8b5ea15c864788cfba35003f8c71161f62456d6
arjava <arjava379@gmail.com>
2022-07-05 14:28:15 +0700

    task with label

--- database/migrations/2022_06_29_065540_create_tasks_table.php
+++ database/migrations/2022_06_29_065540_create_tasks_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('title');
             $table->foreignId('todo_list_id')->constrained()->onDelete('cascade');
             $table->text('description')->nullable();
+            $table->foreignId('label_id')->nullable()->constrained();
             $table->string('status')->default(Task::NOT_STARTED);
             $table->timestamps();
         });
